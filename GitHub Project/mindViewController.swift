87d50123a2341752f6be398f59c00b86fbc545b6
kyley <kspam2go@gmail.com>
2021-07-22 10:09:29 -0700

    created segways, how to, and connected the buttons

---
+++ GitHub Project/mindViewController.swift
@@ -0,0 +1,29 @@
+//
+//  mindViewController.swift
+//  GitHub Project
+//
+//  Created by Hayley Chang on 7/22/21.
+//
+
+import UIKit
+
+class mindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
